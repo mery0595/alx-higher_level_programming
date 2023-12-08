dc6ea3b0dec9a508af492e7d02f25df6374f7a7a
mery0595 <bkmeryem110@gmail.com>
2023-12-08 19:57:31 +0100

    Python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same size as the input matrix
-    result_matrix = [[0 for _ in row] for row in matrix]
-
-    # Iterate through the rows and columns of the input matrix
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            # Compute the square value and store it in the result matrix
-            result_matrix[i][j] = matrix[i][j] ** 2
-
-    return result_matrix
-
-# Example usage:
-input_matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-result = square_matrix_simple(input_matrix)
-print(result)
+    square = []
+    for i in matrix:
+        helper = [e * e for e in i]
+        square.append(helper)
+    return(square)
